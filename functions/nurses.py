54a9b642ea10ac6541f33cb3b0b9eefc40d6893f
dc430 <deepakc3010@gmail.com>
2019-04-03 16:50:10 +0530

    ..

--- functions/nurses.py
+++ functions/nurses.py
@@ -10,10 +10,8 @@ def process_add_new_nurse(req, client):
     res = {}
     print("Request:",req)
     currentId = client.data.nurses.count_documents({})
-    image = req["image"]
-    del req["image"]
     newNurse = req
-    newNurse['_id'] = int(currentId+1)
+    newNurse["_id"] = int(currentId+1)
     newNurse["nop"] = 0
     newNurse["nos1"] = 0
     newNurse["nos2"] = 0
@@ -34,9 +32,8 @@ def process_get_nurse(nurse_id, req, client):
     res = {}
     try:
         nurse = client.data.nurses.find_one({'_id': int(nurse_id)})
-        image = client.images.nurses.find_one({'_id': int(nurse_id)})
         res["fullfilmentText"] = "True"
-        res["data"] = {"data": return_without_password(nurse), "image": image}
+        res["data"] = return_without_password(nurse)
     except:
         res["fullfilmentText"] = "False"
     res["source"] = "hapd-api"
@@ -45,25 +42,15 @@ def process_get_nurse(nurse_id, req, client):
 
 def process_update_nurse(nurse_id, req, client):
     res = {}
-    data = req
     query = {'_id': nurse_id}
     try:
-        if('image' in req):
-            updatedResult = client.images.nurses.update_one(query, {"$set": data['image']})
-            if(updatedResult.raw_result["updatedExisting"] == True):
-                res["fullfilmentText"] = "True"
-                del data["image"]
-            else:
-                res["fullfilmentText"] = "False"
-                res["source"] = "webhook-hapd-api"
-                res = json.dumps(res, indent=4)
-                return res
-        elif('nop' in req):
+        if('nop' in req):
             if(req['operation'] == 'increment'):
                 updatedResult = client.data.nurses.update_one(query, {'$inc': {"nop": 1}})
             else:
-                updatedResult = client.data.nurses.update_one(query, {'$set': {"nop": data["nop"]}})
+                updatedResult = client.data.nurses.update_one(query, {'$set': {"nop": req["nop"]}})
             if(updatedResult.raw_result["updatedExisting"] == True):
+                del req["nop"]
                 res["fullfilmentText"] = "True"
             else:
                 res["fullfilmentText"] = "False"
@@ -73,6 +60,8 @@ def process_update_nurse(nurse_id, req, client):
             else:
                 updatedResult = client.data.nurses.update_one(query, {'$set': {"nos"+req['stage']: req['nos']}})
             if(updatedResult.raw_result["updatedExisting"] == True):
+                del req["stage"]
+                del req["nos"]
                 res["fullfilmentText"] = "True"
             else:
                 res["fullfilmentText"] = "False"
