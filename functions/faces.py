8c03a787c9463d950be7c17b8c4193fce8ed51a8
hapd <47427080+hapd@users.noreply.github.com>
2019-04-25 14:25:45 +0530

    Update faces.py

--- functions/faces.py
+++ functions/faces.py
@@ -1,1 +1,30 @@
 import json
+
+def process_add_face(patient_id, name, client):
+  res = {}
+  result = client.data.faces.find_one({'_id': int(patient_id)})
+  if(result == None):
+    data = {
+      '_id': int(patient_id),
+      'faces': {
+        str(name): 1
+      }
+    }
+  else:
+    faces = result['faces']
+    names = list(faces.keys())
+    if (name not in names):
+      faces[name] = 1
+    else:
+      faces[name] += 1
+    try:
+      updatedResult = client.data.faces.update_one({'_id': int(patient_id)}, {'$set': {'faces': faces}})
+      if(updatedResult.raw_result["updatedExisting"] == True):
+        res["fullfilmentText"] = "True"
+      else:
+        res["fullfilmentText"] = "Could not be updated"
+    except Exception as e:
+      res["fullfilmentText"] = str(e)
+  res["source"] = "hapd-api"
+  res = json.dumps(res, indent=4)
+  return res
